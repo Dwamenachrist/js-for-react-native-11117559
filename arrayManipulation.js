813874f5220046e7e281ba67573eb49638c401e0
Dwamenachrist <agyapongfrancis520@gmail.com>
2024-05-18 22:56:47 +0400

    Add formatArrayStrings to conditionally modify strings based on numbers

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -14,3 +14,37 @@ function processArray(numbers) {
 }
 
 
+
+// Task 2
+
+
+function formatArrayStrings (strings, number) {
+	
+	
+
+	for(let i = 0; i < strings.length; i++) {
+		if ( number[i] % 2 === 0) {
+			strings[i] = strings[i].toUpperCase();
+		} else {
+			strings[i] = strings[i].toLowerCase();
+		}
+	}
+}
+
+const strings = ["Christian", "Kwasi", "Dwamena", "Agyapong"];
+const numbers = processArray([1, 2, 3, 4,]); // Assume this gives [4, 27, 16]
+
+const modifiedStrings = [...strings];
+
+formatArrayStrings(modifiedStrings, numbers);
+
+if (require.main === module )
+console.log(modifiedStrings, numbers); 
+
+
+
+
+module.exports = {
+	strings,
+	modifiedStrings,
+  };
